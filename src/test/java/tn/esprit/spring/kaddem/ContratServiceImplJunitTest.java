601ed6a54556bcb79f4f4fa4bfc62d762d26b721
dorrazorgui <dorra.zorgui@esprit.tn>
2024-10-31 21:36:30 +0100

    testbydorra

--- src/test/java/tn/esprit/spring/kaddem/ContratServiceImplJunitTest.java
+++ src/test/java/tn/esprit/spring/kaddem/ContratServiceImplJunitTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.spring.kaddem.entities.Contrat;
 import tn.esprit.spring.kaddem.entities.Etudiant;
 import tn.esprit.spring.kaddem.entities.Specialite;
@@ -24,6 +26,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
+@RunWith(SpringRunner.class)
+
 public class ContratServiceImplJunitTest {
 
     @Autowired
